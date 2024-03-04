6112c17536145ca8370d9175858a965247f340b6
salim <benayyadabdessalam2000@gmail.com>
2024-03-04 13:42:24 +0100

    add logo to toolbar

--- src/app/air/air.component.ts
+++ src/app/air/air.component.ts
@@ -16,34 +16,34 @@ export class AirComponent {
 
   ngOnInit() {
     // Use interval to trigger the data fetch every 80 seconds
-    this.subscription = interval(30000).pipe(
-      switchMap(() => this.airService.getAirValues())
-    ).subscribe(
-      (values) => {
-        // Update the most recent air quality value
-        this.mostRecentAirValue = values.length > 0 ? values[values.length - 1] : null;
-        console.log('Most Recent Air Quality value:', this.mostRecentAirValue);
-        this.extractedDigits = this.extractTwoDigitsBeforeDecimal(this.mostRecentAirValue);
-        console.log('Extracted Digits:', this.extractedDigits);
-      },
-      (error) => {
-        console.error('Error fetching data:', error);
-      }
-    );
+    // this.subscription = interval(30000).pipe(
+    //   switchMap(() => this.airService.getAirValues())
+    // ).subscribe(
+    //   (values) => {
+    //     // Update the most recent air quality value
+    //     this.mostRecentAirValue = values.length > 0 ? values[values.length - 1] : null;
+    //     console.log('Most Recent Air Quality value:', this.mostRecentAirValue);
+    //     this.extractedDigits = this.extractTwoDigitsBeforeDecimal(this.mostRecentAirValue);
+    //     console.log('Extracted Digits:', this.extractedDigits);
+    //   },
+    //   (error) => {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // );
   }
 
-  ngOnDestroy() {
-    // Unsubscribe to avoid memory leaks
-    this.subscription.unsubscribe();
-  }
+  // ngOnDestroy() {
+  //   // Unsubscribe to avoid memory leaks
+  //   this.subscription.unsubscribe();
+  // }
 
-  private extractTwoDigitsBeforeDecimal(value: number): number {
-    if (value != null) {
-      // Round to two decimal places and convert to a number
-      return +value.toFixed(2);
-    }
+  // private extractTwoDigitsBeforeDecimal(value: number): number {
+  //   if (value != null) {
+  //     // Round to two decimal places and convert to a number
+  //     return +value.toFixed(2);
+  //   }
     
-    // Return null if the value is not valid
-    return null;
-  }
+  //   // Return null if the value is not valid
+  //   return null;
+  // }
 }
